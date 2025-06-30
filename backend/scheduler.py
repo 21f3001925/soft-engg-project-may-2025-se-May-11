e6013121636da810dfdd33ed6bbeb8a3024c2832
aayush <aayushjha4901@gmail.com>
2025-06-30 14:44:36 +0530

    Added shceduling and twilio sms

--- backend/scheduler.py
+++ backend/scheduler.py
@@ -1,5 +1,6 @@
 from apscheduler.schedulers.background import BackgroundScheduler
 from datetime import datetime
+from message import send_sms
 
 
 def send_medication_reminder():
@@ -11,6 +12,10 @@ def send_medication_reminder():
 def start_scheduler():
     scheduler = BackgroundScheduler()
     # Schedule the job (e.g., every day at 9 AM)
-    scheduler.add_job(send_medication_reminder, "cron", hour=19, minute=16)
+    scheduler.add_job(send_medication_reminder, "cron", hour=14, minute=38)
     scheduler.start()
+    send_sms("+916351838115", "Take your Medicine")
+    send_sms("+919884659616", "Take your Medicine")
+    send_sms("+919347714177", "Take your Medicine")
+    send_sms("+919496709986", "Take your Medicine")
     print("Scheduler started.")
