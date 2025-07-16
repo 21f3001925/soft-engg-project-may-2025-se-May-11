0a37badf3f21f4e33ae17df441573b1d1f31f3cf
AryaV <21f1006221@ds.study.iitm.ac.in>
2025-07-17 02:06:02 +0530

    fix: format

--- backend/schemas/medication.py
+++ backend/schemas/medication.py
@@ -1,11 +1,13 @@
 from marshmallow import Schema, fields
 
+
 class MedicationSchema(Schema):
     name = fields.Str(required=True)
     dosage = fields.Str(required=True)
     time = fields.Str(required=True)
     isTaken = fields.Boolean(missing=False)
     senior_id = fields.Int(required=True)
+
 
 class MedicationResponseSchema(Schema):
     medication_id = fields.Int()
@@ -14,6 +16,7 @@ class MedicationResponseSchema(Schema):
     time = fields.Str()
     isTaken = fields.Boolean()
     senior_id = fields.Int()
+
 
 class MedicationAddResponseSchema(Schema):
     message = fields.Str()
--- backend/utils/oauth_setup.py
+++ backend/utils/oauth_setup.py
@@ -1,16 +1,15 @@
 from authlib.integrations.flask_client import OAuth
 from flask import Flask
 
+
 def init_oauth(app: Flask):
     oauth = OAuth(app)
     oauth.register(
-        name='google',
-        client_id=app.config['GOOGLE_CLIENT_ID'],
-        client_secret=app.config['GOOGLE_CLIENT_SECRET'],
+        name="google",
+        client_id=app.config["GOOGLE_CLIENT_ID"],
+        client_secret=app.config["GOOGLE_CLIENT_SECRET"],
         server_metadata_url="https://accounts.google.com/.well-known/openid-configuration",
-        client_kwargs={
-            'scope': 'openid email profile'
-        }
+        client_kwargs={"scope": "openid email profile"},
     )
     app.oauth = oauth
     return oauth
