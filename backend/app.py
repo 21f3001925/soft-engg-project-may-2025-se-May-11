199eb79c016ba36ee34326acdd810953228cfdc9
Ramesh <21f3001925@ds.study.iitm.ac.in>
2025-07-19 14:16:39 +0530

    Fix the wrong import statement

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from backend.utils.oauth_setup import init_oauth
+from utils.oauth_setup import init_oauth
 from dotenv import load_dotenv
 from flask import Flask
 from flask_jwt_extended import JWTManager
