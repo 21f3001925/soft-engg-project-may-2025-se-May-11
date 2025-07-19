45f2aa987bf0ff5791802f163dadac9ebbd1f8ec
RishitPant <rishitpant100@gmail.com>
2025-07-19 14:17:49 +0000

    Fix export

--- backend/celery_app.py
+++ backend/celery_app.py
@@ -1,21 +1,23 @@
-from celery import Celery
-from app import create_app
+# backend/celery_app.py
 
-flask_app = create_app()
+from celery import Celery
+from config import Config
 
 celery_app = Celery(
-    flask_app.import_name,
-    broker=flask_app.config["CELERY_BROKER_URL"],
-    backend=flask_app.config["CELERY_RESULT_BACKEND"],
-    include=["backend.tasks"],  # make sure tasks are imported
+    "senior_app",
+    broker=Config.CELERY_BROKER_URL,
+    backend=Config.CELERY_RESULT_BACKEND,
+    include=["backend.tasks"],  # fully qualified import path
 )
-celery_app.conf.update(flask_app.config)
 
 
-class ContextTask(Celery.Task):
-    def __call__(self, *args, **kwargs):
-        with flask_app.app_context():
-            return self.run(*args, **kwargs)
+def make_celery(app):
+    celery_app.conf.update(app.config)
 
+    class ContextTask(celery_app.Task):
+        def __call__(self, *args, **kwargs):
+            with app.app_context():
+                return self.run(*args, **kwargs)
 
-celery_app.Task = ContextTask
+    celery_app.Task = ContextTask
+    return celery_app
