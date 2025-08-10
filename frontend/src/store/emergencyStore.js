92b2c701dcd23daae184e77a6148c1790c6b3d97
RishitPant <rishitpant100@gmail.com>
2025-08-10 06:39:07 +0000

    Fix lint

--- frontend/src/store/emergencyStore.js
+++ frontend/src/store/emergencyStore.js
@@ -11,9 +11,9 @@ export const useEmergencyStore = defineStore('emergency', {
     async fetchContactsForSenior() {
       const response = await emergencyService.getEmergencyContacts();
       console.log('Fetched contacts:', response.data); // <-- Add this
-      this.contacts = response.data.map(c => ({
+      this.contacts = response.data.map((c) => ({
         ...c,
-        seniorId: c.seniorId ?? c.senior_id
+        seniorId: c.seniorId ?? c.senior_id,
       }));
     },
 
