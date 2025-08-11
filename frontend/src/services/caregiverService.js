3b1423d496ca7de0f1fb5bb74df791f17ded9bee
Rishit Pant <74252265+RishitPant@users.noreply.github.com>
2025-08-11 22:47:01 +0530

    Profile integration (#46)
    * Add timestamp in avatar
    
    * Add stats fields
    
    * Add user specific and stats fields
    
    * Handle crud functions, password and avatar upload
    
    * Set new avatar
    
    * Refactor to dynamically display data, add edit, save profile options
    
    * Change end of line sequence
    
    * Script to populate db with dummy data
    
    * Add auto-increment when missed medication
    
    * Configure to display profile photo
    
    * Add if statements
    
    * Use apiClient.js
    
    * Use apiClient.js
    
    * User apiClient.js
    
    * Use apiClient.js
    
    * Use apiClient.js
    
    * Fetch logged in users emergency contacts
    
    * Show logged in user's emergency contacts
    
    * Fix user profile pic display issue and minor refactoring
    
    * Fix topnav user profile thumbnail display issue and make it DRY
    
    * Fix user fetch on mounting the app
    
    * Fix profile pic upload test case failure
    
    * Fix to check token only if present in localStorage
    
    ---------
    
    Co-authored-by: Ramesh <21f3001925@ds.study.iitm.ac.in>

--- frontend/src/services/caregiverService.js
+++ frontend/src/services/caregiverService.js
@@ -1,26 +1,4 @@
-import axios from 'axios';
-
-const API_URL = 'http://localhost:5000/api/v1';
-
-const apiClient = axios.create({
-  baseURL: API_URL,
-  headers: {
-    'Content-Type': 'application/json',
-  },
-});
-
-apiClient.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem('token');
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  },
-);
+import apiClient from './apiClient';
 
 export default {
   getAssignedSeniors() {
