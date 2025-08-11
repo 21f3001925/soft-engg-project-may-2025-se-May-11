d70a6fe3a54ff926ff8a266018e71f9a12b17efb
RishitPant <rishitpant100@gmail.com>
2025-08-11 18:11:25 +0000

    Make it DRY

--- frontend/src/services/eventService.js
+++ frontend/src/services/eventService.js
@@ -1,26 +1,5 @@
-import axios from 'axios';
+import apiClient from './apiClient';
 
-const API_URL = 'http://localhost:5000/api/v1';
-
-const apiClient = axios.create({
-  baseURL: API_URL,
-  headers: {
-    'Content-Type': 'application/json',
-  },
-});
-
-apiClient.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem('token');
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  },
-);
 export default {
   getEvents() {
     return apiClient.get('/events');
