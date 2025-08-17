bd904dd904b28580395207f2c97b8226cf618112
RishitPant <rishitpant100@gmail.com>
2025-08-17 05:56:35 +0000

    Change port

--- frontend/src/services/reminderService.js
+++ frontend/src/services/reminderService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/v1/reminder';
+const API_URL = 'http://localhost:5001/api/v1/reminder';
 
 const scheduleReminder = (reminderData) => {
   const token = localStorage.getItem('token');
